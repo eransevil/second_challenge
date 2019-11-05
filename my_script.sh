e361621235721478d44b42c6160d9dcee1ed4ab6
eransevil2@gmail.com <eransevil2@gmail.com>
2019-11-05 12:54:30 +0000

    secondchalleng

---
+++ my_script.sh
@@ -0,0 +1,10 @@
+touch file1
+touch file2
+touch file3
+mv file3 file999
+echo "my name is file 1" > file1
+echo "my name is file999" > file999
+echo "my name is file3" > file3
+cp file3 file3_copy
+rm file3
+
